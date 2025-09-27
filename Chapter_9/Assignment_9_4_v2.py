3df22c5a8d3ed36c21d74ae1ddb6d0697fdcf8b5
darby19d <donald.bottorff@gmail.com>
2025-09-27 00:01:48 -0700

    Fourth commit with a version 2 of Assignment_9_4_v2.py to avoid using a list and writing directly to a dictionary

--- Chapter_9/Assignment_9_4_v2.py
+++ Chapter_9/Assignment_9_4_v2.py
@@ -10,7 +10,6 @@ for line in handle:
     if not line.startswith("From "):
         continue
     email = line.split()[1]
-    #elist.append(email)
     ecount[email] = ecount.get(email, 0) + 1
 # identify which emailer is the most and the count
 for emailer,count in ecount.items():
